01f36f8990853058598a28ad9c799100b1df3453
Simon Klein <simon.klein@cloudogu.com>
2021-01-26 17:16:49 +0100

    Remove unnecessary directories in bundle plugin

--- bundle/bundle_plugin.rb
+++ bundle/bundle_plugin.rb
@@ -6,8 +6,6 @@ filenames = Dir.entries("../")
 FileUtils.cp"../Gemfile", PLUGIN_DIR + "/Gemfile", :verbose => true
 FileUtils.cp"../init.rb", PLUGIN_DIR + "/init.rb", :verbose => true
 FileUtils.cp_r "../app/.", PLUGIN_DIR + "/app", :verbose => true
-FileUtils.cp_r "../assets/.", PLUGIN_DIR + "/assets", :verbose => true
 FileUtils.cp_r "../config/.", PLUGIN_DIR + "/config", :verbose => true
-FileUtils.cp_r "../db/.", PLUGIN_DIR + "/db", :verbose => true
 FileUtils.cp_r "../lib/.", PLUGIN_DIR + "/lib", :verbose => true
 
