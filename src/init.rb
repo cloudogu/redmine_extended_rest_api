54b0170490ff82eb454716b5a334780b9a70d96a
Christian Beyer <christian.beyer@cloudogu.com>
2021-02-03 21:32:57 +0100

    fix permission issue for plugin assets #1

--- src/init.rb
+++ src/init.rb
@@ -1,6 +1,6 @@
 Redmine::Plugin.register :redmine_extended_rest_api do
   name 'Extended rest api plugin'
-  author 'Christian Beyer'
+  author 'Christian Beyer (Cloudogu GmbH)'
   author_url 'https://cloudogu.com'
   description 'This plugin extends the existing rest api with new endpoints to manage Redmine.'
   version '1.0.0'
