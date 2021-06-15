858786700701ea621beb9789a0c5058790569c21
Simon Klein <simon.klein@cloudogu.com>
2021-06-15 13:36:21 +0200

    Bump version

--- src/init.rb
+++ src/init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :redmine_extended_rest_api do
   author 'Christian Beyer (Cloudogu GmbH)'
   author_url 'https://cloudogu.com'
   description 'This plugin extends the existing rest api with new endpoints to manage Redmine.'
-  version '1.0.0'
+  version '1.1.0'
 
   settings default: {}
 end
